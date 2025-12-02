e20877c733a930627f25ea823ef843481f62bcf4
lostinechoes <mylilies@gmail.com>
2025-12-02 12:39:54 +0700

    Add Firebase configuration file

---
+++ scripts/firebase-config.js
@@ -0,0 +1,10 @@
+// scripts/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBNIUQpPPas83-Sk8Ux0LX270yZFYSIy0Q",
+  authDomain: "take-the-pills.firebaseapp.com",
+  projectId: "take-the-pills",
+  storageBucket: "take-the-pills.firebasestorage.app",
+  messagingSenderId: "485269196768",
+  appId: "1:485269196768:web:16ddcb88ade8ba404ae585",
+  measurementId: "G-EP5JVDCG1L"
+};
