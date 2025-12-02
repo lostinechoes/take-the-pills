478a0a96499ce9deb16a9e57e9a6deba378e4ea3
lostinechoes <mylilies@gmail.com>
2025-12-02 13:27:47 +0700

    Fix Firebase Storage bucket domain
    Updated the Firebase Storage bucket to the correct domain.

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -3,7 +3,8 @@ export const firebaseConfig = {
   apiKey: "AIzaSyBNIUQpPPas83-Sk8Ux0LX270yZFYSIy0Q",
   authDomain: "take-the-pills.firebaseapp.com",
   projectId: "take-the-pills",
-  storageBucket: "take-the-pills.firebasestorage.app",
+  // corrected: default Firebase Storage bucket uses appspot.com
+  storageBucket: "take-the-pills.appspot.com",
   messagingSenderId: "485269196768",
   appId: "1:485269196768:web:16ddcb88ade8ba404ae585",
   measurementId: "G-EP5JVDCG1L"
